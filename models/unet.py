b35a68a587a6fc36496e6f3de92e5d30cb0d646c
UlkuTuncerKucuktas <ulku.kucutas@radius.ai>
2024-07-06 23:53:46 +0000

    init

--- models/unet.py
+++ models/unet.py
@@ -1,56 +1,115 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchsummary import summary
 
-class UNetEncoder(nn.Module):
-    def __init__(self, input_channels, channel_sizes):
-        super(UNetEncoder, self).__init__()
-        self.encoders = nn.ModuleList()
-        in_channels = input_channels
-        for out_channels in channel_sizes:
-            self.encoders.append(self._block(in_channels, out_channels))
-            in_channels = out_channels
+class DoubleConv(nn.Module):
+    '''(conv => BN => ReLU) * 2'''
 
-    def _block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+    def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True))
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class InConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(InConv, self).__init__()
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class Down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_ch, out_ch)
         )
 
     def forward(self, x):
-        skip_connections = []
-        for encode in self.encoders:
-            x = encode(x)
-            skip_connections.append(x)
-        return x, skip_connections[:-1]
-
-class UNetDecoder(nn.Module):
-    def __init__(self, num_classes, channel_sizes, output_activation=nn.Sigmoid()):
-        super(UNetDecoder, self).__init__()
-        self.decoders = nn.ModuleList()
-        self.output_activation = output_activation
-        reversed_sizes = list(reversed(channel_sizes))
-        for i in range(len(reversed_sizes) - 1):
-            self.decoders.append(self._block(reversed_sizes[i] * 2, reversed_sizes[i + 1]))
-        self.final_conv = nn.Conv2d(reversed_sizes[-1], num_classes, kernel_size=1)
-
-    def _block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x, skip_connections):
-        for i, decode in enumerate(self.decoders):
-            if i < len(skip_connections):
-                x = torch.cat((x, skip_connections[-(i+1)]), dim=1)
-            x = decode(x)
-        x = self.final_conv(x)
-        if self.output_activation:
-            x = self.output_activation(x)
+        x = self.mpconv(x)
         return x
+
+
+class Up(nn.Module):
+    def __init__(self, in_ch, out_ch, bilinear=True):
+        super(Up, self).__init__()
+
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_ch // 2, in_ch // 2, 2, stride=2)
+
+        self.conv = DoubleConv(in_ch, out_ch)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2))
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class Unet(nn.Module):
+    def __init__(self, in_channels, classes,activation="sigmoid"):
+        super(Unet, self).__init__()
+        self.n_channels = in_channels
+        self.n_classes =  classes
+        self.activation = activation
+
+        self.inc = InConv(in_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
+        self.outc = OutConv(64, classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        if self.activation == "sigmoid":
+            x = nn.Sigmoid(x)
+        if self.activation == "softmax":
+            x = nn.Softmax(x)
+        return x
+
