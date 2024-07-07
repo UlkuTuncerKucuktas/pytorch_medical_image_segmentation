5d9f4c0fd586d81c23450916e154ee8eedd01363
UlkuTuncerKucuktas <ulku.kucuktas@radus.ai>
2024-07-07 00:25:14 +0000

    init

--- dataset/image_label_dataset.py
+++ dataset/image_label_dataset.py
@@ -2,19 +2,28 @@ import os
 from PIL import Image
 import torch
 from torch.utils.data import Dataset
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import numpy as np
 
 class ImageLabelDataset(Dataset):
-    def __init__(self, image_folder, label_folder, transform=None, label_transform=None):
+    def __init__(self, image_folder, label_folder, transform=None):
         self.image_folder = image_folder
         self.label_folder = label_folder
         self.transform = transform
-        self.label_transform = label_transform
 
         self.image_files = sorted([f for f in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, f))])
         self.label_files = sorted([f for f in os.listdir(label_folder) if os.path.isfile(os.path.join(label_folder, f))])
 
         self.image_files = [f for f in self.image_files if f in self.label_files]
         self.label_files = [f for f in self.label_files if f in self.image_files]
+
+        if self.transform is None:
+            self.transform = A.Compose([
+                A.Resize(320,640),
+                A.ToFloat(max_value=255.0),  # Ensure images are in [0, 1] range
+                ToTensorV2()
+            ])
 
     def __len__(self):
         return len(self.image_files)
@@ -26,10 +35,12 @@ class ImageLabelDataset(Dataset):
         image = Image.open(img_name).convert('RGB')
         label = Image.open(label_name).convert('L')
 
-        if self.transform:
-            image = self.transform(image)
+        image = np.array(image)
+        label = np.array(label) / 255
 
-        if self.label_transform:
-            label = self.label_transform(label)
+        if self.transform:
+            augmented = self.transform(image=image, mask=label)
+            image = augmented['image']
+            label = augmented['mask']
 
         return image, label
--- train.py
+++ train.py
@@ -14,5 +14,6 @@ test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
